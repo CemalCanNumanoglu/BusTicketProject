6bee87d06166ed0e84b29500b49311ac7fe3beaa
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-04-05 00:12:38 +0300

    Models Created

---
+++ BusTicketProject/Models/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  BusTicketProject
+//
+//  Created by Cemal can Numanoğlu on 3.04.2023.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
