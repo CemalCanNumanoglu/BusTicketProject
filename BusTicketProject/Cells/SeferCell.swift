987f2fdeb428ceb671f48be15cb025d79cc88c71
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-04-09 22:01:17 +0300

    SeferModel Created

---
+++ BusTicketProject/Cells/SeferCell.swift
@@ -0,0 +1,37 @@
+//
+//  SeferCellCode.swift
+//  BusTicketProject
+//
+//  Created by Cemal can Numanoğlu on 9.04.2023.
+//
+
+import UIKit
+
+class SeferCellCode: UITableViewCell{
+    
+    
+    @IBOutlet weak var companyImage: UIImageView!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var deparatureLabel: UILabel!
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configure(model: SeferModel) {
+        self.companyImage.image = UIImage(named: model.companyImage)
+        self.companyNameLabel.text = model.companyName
+        self.deparatureLabel.text = model.deparaturePoint
+        self.destinationLabel.text = model.destinationPoint
+        self.priceLabel.text = model.price
+        self.timeLabel.text = model.time
+    }
+    
+}
---
+++ BusTicketProject/Models/SeferModel.swift
@@ -0,0 +1,17 @@
+//
+//  SeferModel.swift
+//  BusTicketProject
+//
+//  Created by Cemal can Numanoğlu on 9.04.2023.
+//
+import UIKit
+import Foundation
+
+struct SeferModel {
+    let companyName: String
+    let companyImage: String
+    let price: String
+    let deparaturePoint: String
+    let destinationPoint: String
+    let time: String
+}
