db50ce7cade6a4964887ccc1bfe21bbb77b157c2
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-04-12 17:45:34 +0300

    PassengerInfoViewController updated

--- BusTicketProject/Controllers/PassengerInfoViewController/PassengerInfoViewController.swift
+++ BusTicketProject/Controllers/PassengerInfoViewController/PassengerInfoViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class PassengerInfoViewController: UIViewController {
 
+    //MARK: - IBOUTLETS
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var surnameTextField: UITextField!
     @IBOutlet var idTextField: UITextField!
@@ -16,22 +17,23 @@ class PassengerInfoViewController: UIViewController {
     @IBOutlet var priceTextField: UITextField!
     var times = 0
     
-    
+    //MARK: - STORYBOARD FUNCTIONS
     override func viewDidLoad() {
         super.viewDidLoad()
         
         textFieldSettings()
         dataTransfer()
-        
+
         
     }
     
     
-    
+    //MARK: - BUTTON
     @IBAction func takeTicketBtnClicked(_ sender: Any) {
         getData()
     }
     
+    //MARK: - TEXTFIELD SETTINGS FUNC
     private func textFieldSettings() {
         
         let namePlaceHolderText = NSAttributedString(string: "Adınız", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
@@ -44,6 +46,7 @@ class PassengerInfoViewController: UIViewController {
         seatsTextField.isEnabled = false
     }
     
+    //MARK: - DATA TRANSFER FUNC
     private func dataTransfer() {
         let count = UserDefaults.standard.integer(forKey: "count")
         if count != 0 {
@@ -61,12 +64,15 @@ class PassengerInfoViewController: UIViewController {
         }
     }
     
+    //MARK: - GET DATA FUNC
     private func getData() {
         let defaults = UserDefaults.standard
         
         if nameTextField.text == "" || surnameTextField.text == "" || idTextField.text == ""{
             makeAlert(titleInput: "Uyarı!", messageInput: "Lütfen tüm alanları dolduruz.")
+            return
         }
+        
         
         let nameText = nameTextField.text
         let surnameText = surnameTextField.text
@@ -74,11 +80,20 @@ class PassengerInfoViewController: UIViewController {
         let seatText = seatsTextField.text
         let priceText = priceTextField.text
         
-        defaults.set(nameText, forKey: "nameText")
-        defaults.set(surnameText, forKey: "surnameText")
-        defaults.set(idText, forKey: "idText")
+        
+        Passenger.shared.name = nameText
+        Passenger.shared.surname = surnameText
+        Passenger.shared.id = idText
         defaults.set(seatText, forKey: "seatText")
         defaults.set(priceText, forKey: "priceText")
+        
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let displayTicketVC = storyboard.instantiateViewController(withIdentifier: "toDisplayTicket")
+        displayTicketVC.modalPresentationStyle = .fullScreen
+        self.present(displayTicketVC, animated: true)
+        
+        
     }
     
 
