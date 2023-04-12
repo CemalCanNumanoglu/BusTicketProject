d8328100857db3cac984f46e26af61374417d97f
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-04-12 04:11:09 +0300

    SeatCollectionViewCell updated

--- BusTicketProject/Cells/SeatCollectionViewCell.swift
+++ BusTicketProject/Cells/SeatCollectionViewCell.swift
@@ -5,4 +5,11 @@
 //  Created by Cemal can Numanoğlu on 11.04.2023.
 //
 
-import Foundation
+import UIKit
+
+class SeatCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var seatImageView: UIImageView!
+    @IBOutlet var seatNumLabel: UILabel!
+    
+}
