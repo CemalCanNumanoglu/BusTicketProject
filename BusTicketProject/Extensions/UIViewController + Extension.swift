fd6ddf4fd297d62429dc695ba0f61b9507f1ec2e
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-04-12 04:08:02 +0300

    UIViewController + Extension added

--- BusTicketProject/Extensions/UIViewController + Extension.swift
+++ BusTicketProject/Extensions/UIViewController + Extension.swift
@@ -5,4 +5,14 @@
 //  Created by Cemal can Numanoğlu on 11.04.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "Tamam", style: UIAlertAction.Style.cancel)
+        alert.addAction(okButton)
+        self.present(alert,animated: true)
+    }
+}
