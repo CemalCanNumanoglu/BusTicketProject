0d6f7e178876d967a141e06d60d7fe296f17df73
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-04-12 17:46:39 +0300

    DisplayTicketViewController updated

--- BusTicketProject/Controllers/DisplayTicketViewController/DisplayTicketViewController.swift
+++ BusTicketProject/Controllers/DisplayTicketViewController/DisplayTicketViewController.swift
@@ -10,13 +10,44 @@ import UIKit
 class DisplayTicketViewController: UIViewController {
 
     
-
+    @IBOutlet var destinationLabelTxt: UILabel!
+    @IBOutlet var deparatureLabelTxt: UILabel!
+    @IBOutlet var nameLabelTxt: UILabel!
+    @IBOutlet var surnameLabelTxt: UILabel!
+    @IBOutlet var idLabelTxt: UILabel!
+    @IBOutlet var dateLabelTxt: UILabel!
+    @IBOutlet var timeLabelTxt: UILabel!
+    @IBOutlet var seatNumLabelTxt: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let deparature = UserDefaults.standard.string(forKey: "fromWhere") {
+            deparatureLabelTxt.text = deparature
+        }
+        
+        if let destination = UserDefaults.standard.string(forKey: "toWhere") {
+            destinationLabelTxt.text = destination
+        }
+        
+        nameLabelTxt.text = Passenger.shared.name
+        surnameLabelTxt.text = Passenger.shared.surname
+        idLabelTxt.text = Passenger.shared.id
+        dateLabelTxt.text = Ticket.shared.date
+        timeLabelTxt.text = Ticket.shared.time
+        
+        if let selectedSeats = UserDefaults.standard.array(forKey: "selectedSeats"){
+            seatNumLabelTxt.text = "\(selectedSeats)"
+        }
 
     }
     
-
+    
+    @IBAction func homePageBtnClicked(_ sender: Any) {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let homePageVC = storyboard.instantiateViewController(withIdentifier: "homePageVC")
+            homePageVC.modalPresentationStyle = .fullScreen
+            self.present(homePageVC, animated: true)
+        }
 
 }
